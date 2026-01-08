1ba4a530aba36294b716ca9bd6b9abce435b70d5
Aditya Modi <221404511+AdityaModi-1@users.noreply.github.com>
2026-01-07 21:03:25 -0800

    Initial commit - Finance Tracker

---
+++ Main.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Expense{
+    String description;
+    double amount;
+    String category;
+
+    public Expense(String description, double amount, String category){
+        this.description = description;
+        this.amount = amount;
+        this.category = category;
+    }
+
+    public String toString(){
+        return category + ": $" + amount + " (" + description + ")";
+    }
+}
+
+public class Main{
+    static ArrayList<Expense> expenseList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== Personal Finance Tracker ===");
+
+        while (true){
+            System.out.println("\n1. Add Expense");
+            System.out.println("2. All Expenses");
+            System.out.println("3. Exit");
+            System.out.print("Choose an option: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            if (choice == 1){
+                System.out.print("Description (e.g. Coffee): ");
+                String desc = scanner.nextLine();
+
+                System.out.print("Amount (e.g., 5.50): ");
+                double amount = scanner.nextDouble();
+                scanner.nextLine();
+
+                System.out.print("Category. (Food/Rent/Fun): ");
+                String cat = scanner.nextLine();
+
+                expenseList.add(new Expense(desc, amount, cat));
+                System.out.println("Expense Saved!");
+            } else if (choice == 2){
+                System.out.println("\n--- Your Expenses ---");
+                double total = 0;
+                for(Expense e: expenseList){
+                    System.out.println(e.toString());
+                    total += e.amount;
+                }
+                
+                System.out.println("---------------------");
+                System.out.println("TOTAL SPENT: $" + total);
+            } else if(choice == 3){
+                System.out.println("Goodbye!");
+                break;
+            }
+        }
+        scanner.close();
+    }
+}
